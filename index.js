6d1b46daf8bf0450cc4f38d341993867e90fe77d
Shiawase <73775954+MEMEZNUT999@users.noreply.github.com>
2023-07-11 03:17:02 +0300

    Unpatched yet again woohoo

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const args = process.argv;
 
 async function bypass(hwid) {
     const start_url = "https://flux.li/windows/start.php?HWID=" + hwid
-    const header = {
+    const commonheader = {
         'Referer': 'https://linkvertise.com/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
     }
     console.log("Starting bypass")
     await req.request(start_url, {'Referer': 'https://fluxteam.net/'})
+
     // Step 1 (Start)
-    await req.request("https://flux.li/windows/start.php?updated_browser=true&HWID=" + hwid, {'Referer': start_url})
+   await req.request("https://flux.li/windows/start.php?HWID=" + hwid, {'Referer': start_url}) // This was the patch?? lmao
     console.log("\nBypassed checkpoint 1")
     // Step 2
-    await req.request("https://fluxteam.net/windows/checkpoint/check1.php", header)
+    await req.request("https://fluxteam.net/windows/checkpoint/check1.php", commonheader)
     console.log("\nBypassed checkpoint 2")
     // Step 3
-    await req.request("https://fluxteam.net/windows/checkpoint/check2.php", header)
+    await req.request("https://fluxteam.net/windows/checkpoint/check2.php", commonheader)
     // Final (get key)
     const response = await req.request("https://fluxteam.net/windows/checkpoint/main.php",
-        header // "Trying to bypass the Fluxus key system will get you banned from using Fluxus."
+        commonheader // "Trying to bypass the Fluxus key system will get you banned from using Fluxus."
     )
     console.log("\nBypassed final checkpoint & getting key...")
     const parsed = cheerio.load(response['data'])
