f279dbc9f6abbdfb040646d0dd9fe0630cc02112
Shiawase <73775954+MEMEZNUT999@users.noreply.github.com>
2023-07-17 20:51:13 +0300

    removed useless comments

--- v2/main.py
+++ v2/main.py
@@ -5,7 +5,6 @@
  / V2 version, runnning into errors? Make an issue!
 
 '''
-# Packages
 import sys
 import time
 from seleniumwire import webdriver
@@ -13,10 +12,9 @@ from seleniumwire.utils import decode
 from bs4 import BeautifulSoup
 from colorama import Fore, Back, Style, init
 
-# colorama windows
+# colorama for windows, will not affect other platforms
 init()
 
-# get key
 def getkey(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     code_elements = soup.find_all('code')
@@ -58,29 +56,23 @@ def main():
     print(Style.RESET_ALL)
     base_url = f"https://flux.li/windows/start.php?HWID={hwid}"
 
-    # options
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("log-level=3")
     driver = webdriver.Chrome(options=options)
 
     try:
-        # init
         driver.get(base_url)
         time.sleep(5)
-        # wow bypass so cool
+
         driver.header_overrides = {'Referer': 'https://linkvertise.com'}
 
         driver.get(f"{base_url}&7b20bcc1dfe26db966bb84f159da392f=false")
-        # 1
         driver.get("https://fluxteam.net/windows/checkpoint/check1.php")
         time.sleep(3) 
-        # 2
         driver.get("https://fluxteam.net/windows/checkpoint/check2.php")
         time.sleep(3) 
-        # last
         driver.get("https://fluxteam.net/windows/checkpoint/main.php")
-        # Get key
         for request in driver.requests:
             if request.url == "https://fluxteam.net/windows/checkpoint/main.php":
                 body = decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity'))
