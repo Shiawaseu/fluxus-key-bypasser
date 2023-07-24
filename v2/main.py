5d25f4c40cce308cbbd5cf54d78309d7404dc429
Simon1907 <78481748+Simon1907@users.noreply.github.com>
2023-07-24 16:37:37 +0200

    Update main.py

--- v2/main.py
+++ v2/main.py
@@ -1,93 +1,100 @@
-'''
+# Ive used threading :)
 
- / Github: https://github.com/MEMEZNUT999/fluxus-key-bypasser
-
- / V2 version, runnning into errors? Make an issue!
-
-'''
-import sys
+import subprocess
+state = True
+def install(package_name):
+    try:
+        subprocess.run(["pip", "install", package_name], check=True,  stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        print(f"Package {package_name} installed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing {package_name}: {e}")
+print("Checking for missing packages")
+try: 
+    import requests
+except ImportError:
+    print("Installing requests ... ")
+    install("requests")
+    print("Installed requests!")
+    import requests
+try: 
+    import keyboard
+except ImportError:
+    print("Installing keyboard ... ")
+    install("keyboard")
+    import keyboard
+    print("Installed keyboard!")
+try: 
+    from bs4 import BeautifulSoup
+except ImportError:
+    print("Installing BeautifulSoup4 ... ")
+    install("beautifulsoup4")
+    print("Installed BeautifulSoup4!")
+    from bs4 import BeautifulSoup
+print("Done!\n")
+import threading as thread
 import time
-from seleniumwire import webdriver
-from seleniumwire.utils import decode
-from bs4 import BeautifulSoup
-from colorama import Fore, Back, Style, init
-from fake_useragent import UserAgent
+THREAD = True
+COMMON_COOKIE = "Anti-Bypass=BypassersKHTTP_VERSION5069e4e61337c2fbea2368f9da1a07725f2a65bb1eab2d8de6dc9cf83e7a683e; .pipe=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJLMGc4SjNsRmY1TW43UWw4bVh5bytpNnVBeGh4aWFSYTU2bldDZEcxQnlNPSIsImUiOjE2ODkyNTAyODEsImlzc3VlZCI6MTY4OTI0NjY4MS44MzksInNhbHQiOiJzYWx0eSIsImNvbm5lY3RvciI6LTF9.tHnUGnosgCctAafGTgta4F1_1KQezhvdIATrj9YwQU0"
+COMMON_HEADER = {
+    'Referer': 'https://linkvertise.com/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
+}
 
-# colorama for windows, will not affect other platforms
-init()
+def req(url, headers):
+    if THREAD == True:
+        return requests.get(url, headers=headers)
+    return False
 
-def getkey(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    code_elements = soup.find_all('code')
-    if len(code_elements) > 1:
-        return code_elements[1].text.strip()
-    return None
+def bypass(hwid):
+    if THREAD == True:
+        start_url = f"https://flux.li/windows/start.php?HWID={hwid}"
+        print("Bypassing. Please be patient!")
+        if req(start_url, headers=COMMON_HEADER) == False: return h()
 
-def readhwid(filename):
+        if req(f"https://flux.li/windows/start.php?7b20bcc1dfe26db966bb84f159da392f=false&HWID={hwid}",
+                     headers={'Referer': start_url, 'Cookie': COMMON_COOKIE}) == False: return h()
+        if req("https://fluxteam.net/windows/checkpoint/check1.php", headers=COMMON_HEADER) == False: return h()
+        if req("https://fluxteam.net/windows/checkpoint/check2.php", headers=COMMON_HEADER) == False: return h()
+        response = req("https://fluxteam.net/windows/checkpoint/main.php", headers=COMMON_HEADER)
+        if response == False: return h()
+        parsed = BeautifulSoup(response.content, 'html.parser')
+        key = parsed.select("body > main > code:nth-child(5)")[0].text.strip()
+        print("Fluxus key has been activated! \nKey: " + key + '\n')
+        input("Fun fact: Fluxus key is always static, it rarely gets changed. \nIf you dont get to see a key, its very likely patched. ")
+        exit()
+
+def h():
+    global state
     try:
-        with open(filename, 'r') as file:
-            return file.read().strip()
+        with open("hwid.txt", "r+") as f:
+            hwid = f.readline().strip()
+            if not hwid:
+                state = False
+                hwid = input("Enter HWID: ")
+                f.seek(0)
+                f.write(hwid)
+                state = True
     except FileNotFoundError:
-        return None
+        with open("hwid.txt", "w") as f:
+            state = False
+            hwid = input("Enter HWID: ")
+            f.write(hwid)
+            state = True
+    bypass(hwid)
 
-def writehwid(filename, hwid):
-    with open(filename, 'w') as file:
-        file.write(hwid)
-
-def gethwid():
-    filename = "savedhwid.bypass"
-
-    hwid = readhwid(filename)
-    if hwid is not None:
-        print(Fore.GREEN + "[LOG] HWID found in the file. Using that.")
-        return hwid
-    else:
-        print(Fore.YELLOW + "[WARN] No HWID found in the file.")
-        hwid = input("[PROMPT] Enter your HWID: ")
-        writehwid(filename, hwid)
-        print(Fore.GREEN + "[LOG] HWID saved to the file. Running program now.")
-        return hwid
-
-
-def main():
-    print(Fore.CYAN + "[*] Please support me by starring the repo!")
-    time.sleep(1)
-    hwid = gethwid()
-    print(Fore.WHITE + "[!] Program is starting, if this takes longer than 1 minute, restart.")
-    print(Style.RESET_ALL)
-    base_url = f"https://flux.li/windows/start.php?HWID={hwid}"
-    UA = UserAgent()
-    agent = UA.random
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new") # https://www.selenium.dev/blog/2023/headless-is-going-away/
-    options.add_argument(f"user-agent={agent}")
-    options.add_argument("window-size=1400,600")
-    options.add_argument("log-level=3")
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(options=options)
-
-    try:
-        driver.get(base_url)
-        time.sleep(5)
-        driver.header_overrides = {'Referer': base_url}
-        driver.get(f"{base_url}&7b20bcc1dfe26db966bb84f159da392f=false")
-        driver.header_overrides = {'Referer': 'https://linkvertise.com'}
-        driver.get("https://fluxteam.net/windows/checkpoint/check1.php")
-        time.sleep(3) 
-        driver.get("https://fluxteam.net/windows/checkpoint/check2.php")
-        time.sleep(3) 
-        driver.get("https://fluxteam.net/windows/checkpoint/main.php")
-        for request in driver.requests:
-            if request.url == "https://fluxteam.net/windows/checkpoint/main.php":
-                body = decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity'))
-                key = getkey(body)
-                print(Fore.GREEN + f"[LOG] Your key is: {key}")
-
-    except Exception as e:
-        print(Fore.RED + f"An error occurred: {e}")
-
-    finally:
-        driver.quit()
+def wipe():
+    if state == True:
+        global THREAD
+        with open("hwid.txt", "w") as f:
+                    f.write("")
+        THREAD = False
+        print("Wiped HWID, Restarting")
+        time.sleep(1)
+        THREAD = True
+        h()
 
 if __name__ == "__main__":
-    main()
+    keyboard.add_hotkey("c", wipe)
+    print("PRESS 'C' TO WIPE OUT THE HWID SO YOU CAN USE ANOTHER ONE (only works while bypassing old hwid lol)")
+    h()
+    t1 = thread.Thread(target=bypass)
